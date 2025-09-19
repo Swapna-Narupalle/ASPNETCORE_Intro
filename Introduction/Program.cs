b76eed2548771c9b5f885681578be2e4ffc27a39
Narupalle_swapna <nswapna.fullstack@gmail.com>
2025-09-19 17:41:35 +0530

    Add project files.

---
+++ Introduction/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddCors((cors) =>
+{
+    cors.AddPolicy("AllowLocalhost4200", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200").
+        AllowAnyHeader().
+        AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseCors("AllowLocalhost4200");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
