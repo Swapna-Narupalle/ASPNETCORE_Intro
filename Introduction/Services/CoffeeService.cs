3113e66638bf31acdca1ee05a96c8c879e0f1387
Narupalle_swapna <nswapna.fullstack@gmail.com>
2025-09-26 00:39:38 +0530

    ServiceLifeTimes

---
+++ Introduction/Services/CoffeeService.cs
@@ -0,0 +1,24 @@
+﻿namespace Introduction.Interfaces;
+
+public class CoffeeService : ISingletonCoffee, IScopedCoffee, ITransientCoffee
+{
+    private readonly string _coffeeId;
+
+
+    public CoffeeService()
+    {
+        _coffeeId = Guid.NewGuid().ToString();
+    }
+
+
+    public string GetCoffeeId()
+    {
+        return _coffeeId;
+    }
+
+
+}
+
+
+
+
