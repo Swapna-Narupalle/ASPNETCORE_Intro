36a6c237439f2fbea13b24218acd9c86a5971eb4
Narupalle_swapna <nswapna.fullstack@gmail.com>
2025-10-09 13:59:40 +0530

    ActionFilters

---
+++ Introduction/Controllers/ReportsController.cs
@@ -0,0 +1,33 @@
+﻿using Introduction.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Introduction.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+
+    [RequireDomainEmailAttribute]
+    public class ReportsController : ControllerBase
+    {
+        //https://localhost:7233/api/Reports/daily
+        [HttpGet("daily")]
+        public IActionResult GetDaily([FromHeader(Name = "email")] string email)
+        {
+            return Ok(new { Message = "Daily report generated.", Email = email });
+        }
+
+        [HttpGet("monthly")]
+        public IActionResult GetMonthly([FromHeader(Name = "email")] string email)
+        {
+            return Ok(new { Message = "Monthly report generated.", Email = email });
+        }
+
+
+        [HttpGet("Yearly")]
+        public IActionResult GetYearly([FromHeader(Name = "email")] string email)
+        {
+            return Ok(new { Message = "Yearly report generated.", Email = email });
+        }
+    }
+}
